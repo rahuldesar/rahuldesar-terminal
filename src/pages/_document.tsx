0e60f1277d83800758aec0549a2800a21bd17158
Rahul Desar <desarrahul@gmail.com>
2024-04-26 07:16:07 +0545

    default cleanups

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="flex min-h-full items-center">
         <Main />
         <NextScript />
       </body>
