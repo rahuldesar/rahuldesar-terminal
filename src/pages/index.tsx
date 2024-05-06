f729eef908c6f5fd9e8200983513eacae4582119
Rahul Desar <desarrahul@gmail.com>
2024-05-06 23:00:51 +0545

    refactor terminal with theme switcher

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,22 @@
-/* eslint-disable react/no-unescaped-entities */
-import PromptHeader from "@/components/PromptHeader";
-import Welcome from "@/components/Welcome";
-import { useEffect, useState } from "react";
+import Terminal from "@/components/Terminal";
+import { useTheme } from "@/hooks/useTheme";
+import { createContext, useContext, useEffect, useState } from "react";
+
+export const ThemeContext = createContext<((theme: string) => void) | null>(
+  null
+);
 
 export default function Home() {
-  const [inputText, setInputText] = useState("hello there");
-  const [loading, setLoading] = useState(true);
+  const { isThemeLoaded, setTheme } = useTheme();
 
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 1500);
-  }, [loading]);
+  const updateTheme = (theme: string) => {
+    setTheme(theme);
+  };
 
-  if (loading) {
-    return (
-      <div className="p-5 text-primary">
-        connecting to server
-        <div className="loading-animation inline">
-          <span className="dot">.</span>
-          <span className="dot">.</span>
-          <span className="dot">.</span>
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <section className="p-5">
-          <Welcome />
-          <div className="">
-            <PromptHeader />
-            <input
-              type="text"
-              className="pl-2"
-              onChange={(e) => setInputText(e.target.value)}
-              value={inputText}
-            />
-          </div>
-        </section>
-      </div>
-    );
-  }
+  if (!isThemeLoaded) return <></>;
+  return (
+    <ThemeContext.Provider value={updateTheme}>
+      <Terminal />
+    </ThemeContext.Provider>
+  );
 }
